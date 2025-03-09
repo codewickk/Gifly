5b7e04bd2cc5cdc237ff953ba027ac1602ee8653
codewickk <codewickk@gmail.com>
2025-03-09 14:58:28 +0530

    port

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,8 @@ const sharp = require("sharp");
 const cors = require('cors'); 
 const archiver = require('archiver'); 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 app.use(express.json()); 
 app.use(cors()); 
