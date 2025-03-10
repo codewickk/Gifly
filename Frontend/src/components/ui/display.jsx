00b90d0cbbbac467b0ae56c2b4113ed6e80c217b
codewickk <codewickk@gmail.com>
2025-03-10 13:25:20 +0530

    route changes

--- Frontend/src/components/ui/display.jsx
+++ Frontend/src/components/ui/display.jsx
@@ -16,7 +16,7 @@ const Display = () => {
   const [currentQuoteIndex, setCurrentQuoteIndex] = useState(0); 
 
  
-  const API_BASE_URL = "";
+  const API_BASE_URL = "https://gifly.onrender.com";
 
 
   const patienceQuotes = [
